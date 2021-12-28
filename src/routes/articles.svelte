c577fd2f67728b8db404d5f3e64183564c2b90e8
Andrew Carbungco <andrew.carbungco@gmail.com>
2021-12-27 22:59:07 -0800

    Add files via upload

---
+++ src/routes/articles.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Nav from "../Components/nav.svelte";
+</script>
+
+<section id="articleSection">
+    <Nav/>
+    <section></section>
+</section>
+
+<style>
+    #articleSection {
+    display: grid;
+    background-size: cover;
+    background: #222634;
+    height: 100vh;
+    grid-template-columns: 15% 85%;
+    font-family: 'PT Sans Narrow', sans-serif;
+}
+</style>
