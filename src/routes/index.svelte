823ffdc622ee62520f5fd189fa461bc6de04b93d
Andrew Carbungco <andrew.carbungco@gmail.com>
2022-08-31 22:32:27 -0700

    Add files via upload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,20 @@
     let hpArticle = "images/Home Page/HP UI Case Study.pdf";
 </script>
 
+<head>
+    <!-- Hotjar Tracking Code for https://drew-by-design.netlify.app -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3134551,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+</head>
+
 <div id="homePage">
     <Nav/>
     <section id="main">
